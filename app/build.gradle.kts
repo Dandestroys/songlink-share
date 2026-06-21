04b660ffe4beafc520d656a07bd2bacc12f0c45c
lillythenova <higginsdm23@gmail.com>
2026-06-21 20:27:12 +0100

    Optimize: make debug APK as small as possible by enabling minification and R8 for debug build type

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,9 @@ android {
         versionCode = 1
         versionName = "1.0"
         resourceConfigurations += "en"
-
+        ndk {
+            abiFilters.add("arm64-v8a")
+        }
     }
 
     signingConfigs {
@@ -41,6 +43,15 @@ android {
             } else {
                 signingConfigs.getByName("debug")
             }
+        }
+        debug {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 
